3f0842b9085284bb81f2bd1c0f3eed12b6173fc3
sanish <s.karki1994@gmail.com>
2025-05-16 16:05:06 +0545

    [revious button , curretn button, next button COMPLETED

--- src/component/Pagination.jsx
+++ src/component/Pagination.jsx
@@ -1,27 +1,98 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import '../component/Pagination.css';
 
 const Pagination = () => {
   const [tabelData, setTableData] = useState();
 
   const [currentPage, setCurrentPage] = useState(1);
+
   const [itemsPerPage, setItemsPerPage] = useState(10);
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
 
+  const totalPages = Math.ceil(tabelData?.total / itemsPerPage);
+
+  // Actual users data from API -- this runs before API Data is received so, [empty array] is used
+  const currentUsersData = tabelData?.users.slice(startIndex, endIndex) || [];
+
   useEffect(() => {
     axios.get('https://dummyjson.com/users?limit=0').then((res) => {
-      // console.log(res);
+      console.log(res);
       setTableData(res?.data);
     });
   }, []);
 
-  // Actual users data from API
-  const usersData = tabelData?.users.slice(startIndex, endIndex);
-  console.log(usersData);
+  // ------------PREV / NEXT button functionality--------------------
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
 
-  return <>Pagination</>;
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  //----IMP: set currentPage value
+  const handlePageClick = (pageNo) => {
+    setCurrentPage(pageNo);
+  };
+
+  return (
+    <>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {currentUsersData.map((value, index) => {
+            return (
+              <tr key={index}>
+                <td>{value.firstName}</td>
+                <td>{value.email}</td>
+                <td>{value.gender}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {/* ------------------BUTTONS---------------- */}
+      <div className='pagination'>
+        <button onClick={() => handlePrevious()} disabled={currentPage === 1}>
+          Previous
+        </button>
+
+        {Array.from({ length: totalPages }, (_, index) => {
+          return (
+            //----IMP: set currentPage value
+            <button
+              onClick={() => handlePageClick(index + 1)}
+              className={currentPage === index + 1 ? 'active' : ''}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+
+        <button
+          onClick={() => handleNext()}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Pagination;
